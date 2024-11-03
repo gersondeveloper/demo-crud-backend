f7a112a01a734c070f4898549e75b11d9f29e80a
Gerson C Filho <gersoncfilho@mac.com>
2024-11-03 13:16:12 +0000

    refact: refactored UserController.java

--- src/main/java/com/badge/democrud/backend/demo_crud_backend/controller/UserController.java
+++ src/main/java/com/badge/democrud/backend/demo_crud_backend/controller/UserController.java
@@ -6,14 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/")
+@CrossOrigin(value = "http://localhost:4200")
 public class UserController {
 
     private final UserService userService;
@@ -21,7 +19,8 @@ public class UserController {
     @GetMapping("users")
     public Page<User> getUsers(@RequestParam int page, @RequestParam int size) {
         Pageable pageable = PageRequest.of(page, size);
-        return userService.findAll(pageable);
+        var users = userService.findAll(pageable);
+        return users;
     }
 
 //    @GetMapping("api/users/{id}")
