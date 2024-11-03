6c8df33e32b883dff1bcf68549da7f71eff572b5
gerson <gerson@debian.Home>
2024-11-03 11:34:03 +0000

    Initial commit

---
+++ src/main/java/com/badge/democrud/backend/demo_crud_backend/DemoCrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.badge.democrud.backend.demo_crud_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrudBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/badge/democrud/backend/demo_crud_backend/DemoCrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badge.democrud.backend.demo_crud_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
